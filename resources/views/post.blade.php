032727e894e3acb434736d1712a2cb06bf424dd2
danargh <danargh86@gmail.com>
2023-08-15 09:21:04 +0700

    feat: adding blade template to home etc

---
+++ resources/views/post.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+
+@section('container')
+<article class=" mt-5">
+   <h2>ini</h2>
+   <h5>ini</h5>
+   <p>ini</p>
+</article>
+<a href="/posts">Back to posts</a>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,41 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', [
+        "title" => "home"
+    ]);
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "title" => "about",
+        "name" => "Danar Ghulamsyah",
+        "email" => "danargh86@gmail.com",
+        "image" => "danar.jpg"
+    ]);
+})->name('about');
+
+Route::get('/posts', function () {
+    $blog_posts = [
+        [
+            "title" => "Judul post pertama",
+            "author" => "Danargh",
+            "slug" => "judul-post-pertama",
+            "body" => "Elit cupidatat nostrud fugiat velit nisi commodo officia deserunt cupidatat labore nostrud in quis voluptate. Nisi dolor incididunt fugiat do. Cupidatat voluptate nisi veniam tempor magna Lorem do ea ut duis amet. Aliqua id ad pariatur adipisicing ut quis excepteur consectetur aliqua sit irure."
+        ],
+        [
+            "title" => "Judul post kedua",
+            "author" => "Danargh",
+            "slug" => "judul-post-kedua",
+            "body" => "Elit cupidatat nostrud fugiat velit nisi commodo officia deserunt cupidatat labore nostrud in quis voluptate. Nisi dolor incididunt fugiat do. Cupidatat voluptate nisi veniam tempor magna Lorem do ea ut duis amet. Aliqua id ad pariatur adipisicing ut quis excepteur consectetur aliqua sit irure."
+        ]
+    ];
+    return view('posts', [
+        "title" => "posts", "posts" => $blog_posts
+    ]);
+})->name('posts');
+
+// halaman single post
+Route::get('posts/{slug}', function ($slug) {
+    return view('post', ['title' => 'Single post']);
 });
